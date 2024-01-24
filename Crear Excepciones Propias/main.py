4c9fc2cc05de789248be78f632de7222527d6953
lexicon06 <pablo.hehe2@gmail.com>
2024-01-24 00:34:28 -0300

    error handling with class arguments

--- Crear Excepciones Propias/main.py
+++ Crear Excepciones Propias/main.py
@@ -1,21 +1,31 @@
 class UserNameException(Exception):
     def __init__(self):
         self.message = "Cody no puede ser tu nombre"
+        super().__init__(self.message)
+
+class UserNameLenException(Exception):
+    def __init__(self, name):
+        self.message = f"User {name} needs to have at least 3 characters"
+        super().__init__(self.message)
+
 
 
 def validate_username(name):
     if name.lower() == "cody":
         raise UserNameException()
     if len(name) < 3:
-        raise Exception("Tiene que tener mas de 3 caracteres")
+        raise UserNameLenException(name)
     
     return True
 
 
 try:
-    response:bool = validate_username("Cody")
+    response:bool = validate_username("Cod")
 
 except Exception as e:
     print("Custom errors")
-    print(e.message)
+    print(e)
+
+else:
+    print("User is valid")
 
