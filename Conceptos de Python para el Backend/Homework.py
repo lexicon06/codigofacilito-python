f1610a8ad82fcc9164836b7d33236d4db2b1c04c
lexicon06 <pablo.hehe2@gmail.com>
2024-01-24 13:27:04 -0300

    homework done

---
+++ Conceptos de Python para el Backend/Homework.py
@@ -0,0 +1,22 @@
+def deco_validator(func):
+    def wrapper(a:int,b:int):
+        if a % 2 != 0 and b % 2 != 0:
+            return func(a, b)
+        else:
+            raise ValueError("Number is even")
+
+    return wrapper
+
+@deco_validator
+def add(a:int, b:int):
+    return a+b
+   
+
+try:
+    result = add(1,2)
+except Exception as error:
+    print(f"Error {error} has been found")
+else:
+    print(f"Result is {result}")
+finally:
+    print(f"Code has ended")
