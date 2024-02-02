32e47071045072944cf0cf3e465adbc20f490912
lexicon06 <pablo.hehe2@gmail.com>
2024-02-01 22:48:43 -0300

    Login app in flask

--- Bases de Datos con Flask - Generación 2/practice/Flask2/app.py
+++ Bases de Datos con Flask - Generación 2/practice/Flask2/app.py
@@ -7,3 +7,14 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/success", methods=['GET'])
+def check():
+    user = request.args.get("uname")
+    password = request.args.get("pwd")
+    return render_template("success.html", usr=user, pwd=password)
