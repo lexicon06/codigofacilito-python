5344dd177e458ee98704f42ef4f7264a6bc45167
lexicon06 <pablo.hehe2@gmail.com>
2024-03-26 23:28:39 -0300

    boiler plate for more

--- Introducción  a Django/django2024/tiendita/products/models.py
+++ Introducción  a Django/django2024/tiendita/products/models.py
@@ -7,3 +7,11 @@ class Poduct(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Courses(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+
+    def __str__(self):
+        return self.title
